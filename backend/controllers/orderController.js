68703956682f98ed1395725e1f8b519be0ca7d4f
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-12 08:45:11 +0800

    order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,112 @@
+const Order = require('../models/order');
+const Product = require('../models/product');
+
+exports.newOrder = async (req, res, next) => {
+    const {
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo
+
+    } = req.body;
+
+    const order = await Order.create({
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+        paidAt: Date.now(),
+        user: req.user._id
+    })
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+}
+
+exports.getSingleOrder = async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+    if (!order) {
+        return res.status(404).json({ message: `No Order found with this ID` })
+
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+}
+
+exports.myOrders = async (req, res, next) => {
+    const orders = await Order.find({ user: req.user.id })
+
+    res.status(200).json({
+        success: true,
+        orders
+    })
+}
+
+exports.allOrders = async (req, res, next) => {
+    const orders = await Order.find()
+
+    let totalAmount = 0;
+
+    orders.forEach(order => {
+        totalAmount += order.totalPrice
+    })
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders
+    })
+}
+
+exports.updateOrder = async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order.orderStatus === 'Delivered') {
+        return res.status(404).json({ message: `You have already delivered this order` })
+
+    }
+
+    order.orderItems.forEach(async item => {
+        await updateStock(item.product, item.quantity)
+    })
+
+    order.orderStatus = req.body.status
+    order.deliveredAt = Date.now()
+    await order.save()
+
+    res.status(200).json({
+        success: true,
+    })
+}
+
+async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+    product.stock = product.stock - quantity;
+    await product.save({ validateBeforeSave: false })
+}
+
+exports.deleteOrder = async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+
+    if (!order) {
+        return res.status(404).json({ message: `No Order found with this ID` })
+     
+    }
+    await order.remove()
+
+    res.status(200).json({
+        success: true
+    })
+}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logout, forgotPassword, 
+const { registerUser, loginUser, logout, forgotPassword,
     resetPassword,
     getUserProfile,
     updatePassword,
@@ -11,10 +11,11 @@ const { registerUser, loginUser, logout, forgotPassword,
 
 
 } = require('../controllers/authController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
-router.post('/register',registerUser);
+router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/logout',logout);
+router.get('/logout', logout);
 
 router.post('/password/forgot', forgotPassword);
 router.put('/password/reset/:token', resetPassword);
@@ -22,6 +23,6 @@ router.get('/me', isAuthenticatedUser, getUserProfile)
 router.put('/password/update', isAuthenticatedUser, updatePassword)
 router.put('/me/update', isAuthenticatedUser, updateProfile)
 router.get('/admin/users', isAuthenticatedUser, allUsers)
-router.get('/admin/user/:id',isAuthenticatedUser, getUserDetails )
+router.get('/admin/user/:id', isAuthenticatedUser, getUserDetails)
 
 module.exports = router;
