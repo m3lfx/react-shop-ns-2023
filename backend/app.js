554b5abad7471c5e2e6162c37a484d439e43c5de
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-12 09:21:41 +0800

    app css

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ const app = express();
 const cookieParser = require('cookie-parser')
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
+app.use('/api/v1', order);
+
 
 
 module.exports = app
