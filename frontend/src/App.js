f0a4e7b98bbee9948aa7d3c7cba749f83177e098
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-12 09:39:34 +0800

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Components/Layout/Header'
 import Footer from './Components/Layout/Footer'
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} exact="true" />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,34 +5,34 @@ const Home = () => {
     return (
         <Fragment>
             <MetaData title={'Buy Best Products Online'} />
-        <div className="container container-fluid">
-            <h1 id="products_heading">Latest Products</h1>
-            <section id="products" className="container mt-5">
-                <div className="row">
-                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                        <div className="card p-3 rounded">
-                            <img
-                                className="card-img-top mx-auto"
-                                src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
-                            />
-                            <div className="card-body d-flex flex-column">
-                                <h5 className="card-title">
-                                    <a href="">128GB Solid Storage Memory card - SanDisk Ultra</a>
-                                </h5>
-                                <div className="ratings mt-auto">
-                                    <div className="rating-outer">
-                                        <div className="rating-inner"></div>
+            <div className="container container-fluid">
+                <h1 id="products_heading">Latest Products</h1>
+                <section id="products" className="container mt-5">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                            <div className="card p-3 rounded">
+                                <img
+                                    className="card-img-top mx-auto"
+                                    src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
+                                />
+                                <div className="card-body d-flex flex-column">
+                                    <h5 className="card-title">
+                                        <a href="">128GB Solid Storage Memory card - SanDisk Ultra</a>
+                                    </h5>
+                                    <div className="ratings mt-auto">
+                                        <div className="rating-outer">
+                                            <div className="rating-inner"></div>
+                                        </div>
+                                        <span id="no_of_reviews">(5 Reviews)</span>
                                     </div>
-                                    <span id="no_of_reviews">(5 Reviews)</span>
+                                    <p className="card-text">$45.67</p>
+                                    <a href="#" id="view_btn" className="btn btn-block">View Details</a>
                                 </div>
-                                <p className="card-text">$45.67</p>
-                                <a href="#" id="view_btn" className="btn btn-block">View Details</a>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
         </Fragment>
     )
 }
