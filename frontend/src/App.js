de1d0cab6872f368d26f91eb1b6fd0464fb06c87
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-17 09:16:47 +0800

    login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Layout/Header'
 import Footer from './Components/Layout/Footer'
 import Home from './Components/Home'
 import ProductDetails from './Components/Product/ProductDetails'
+import Login from './Components/User/Login';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} exact="true" />
           <Route path="/product/:id" element={<ProductDetails />} exact="true" />
           <Route path="/search/:keyword" element={<Home />} exact="true" />
+
+          <Route path="/login" element={<Login />} exact="true"/>
         </Routes>
         <Footer />
       </Router>
---
+++ frontend/src/Components/User/Login.js
@@ -0,0 +1,99 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+
+import Loader from '../Layout/Loader'
+import Metadata from '../Layout/Metadata'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+
+const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false)
+    
+    const navigate = useNavigate()
+    let location = useLocation();
+    // const redirect = location.search ? new URLSearchParams(location.search).get('redirect') : ''
+    const notify = (error) => toast.error(error, {
+        position: toast.POSITION.BOTTOM_RIGHT
+    });
+
+    // const login = async (email, password) => {
+    //     try {
+    //         const config = {
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         }
+    //         const { data } = await axios.post(`${process.env.REACT_APP_API}/api/v1/login`, { email, password }, config)
+    //         console.log(data)
+    //     } catch (error) {
+    //             toast.error(error.response.data.message)
+    //     }
+    // }
+    
+
+    // const submitHandler = (e) => {
+    //     e.preventDefault();
+    //     login(email, password)
+    // }
+
+    return (
+        <Fragment>
+            {loading ? <Loader /> : (
+                <Fragment>
+                    <Metadata title={'Login'} />
+
+                    <div className="row wrapper">
+                        <div className="col-10 col-lg-5">
+                            <form className="shadow-lg" 
+                            // onSubmit={submitHandler}
+                            >
+                                <h1 className="mb-3">Login</h1>
+                                <div className="form-group">
+                                    <label htmlFor="email_field">Email</label>
+                                    <input
+                                        type="email"
+                                        id="email_field"
+                                        className="form-control"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="password_field">Password</label>
+                                    <input
+                                        type="password"
+                                        id="password_field"
+                                        className="form-control"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+
+                                <Link to="/password/forgot" className="float-right mb-4">Forgot Password?</Link>
+
+                                <button
+                                    id="login_button"
+                                    type="submit"
+                                    className="btn btn-block py-3"
+                                >
+                                    LOGIN
+                                </button>
+
+                                <Link to="/register" className="float-right mt-3">New User?</Link>
+                            </form>
+                        </div>
+                    </div>
+
+
+                </Fragment>
+            )}
+        </Fragment>
+    )
+}
+
+export default Login
