de7e19500dfb0e30d9909bbbe9ca1cb865e12f60
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-17 09:19:12 +0800

    login link header

--- frontend/src/Components/Layout/Header.js
+++ frontend/src/Components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import '../../App.css'
 import Search from './Search'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 
@@ -17,7 +18,7 @@ const Header = () => {
                 </div>
 
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                    <button className="btn" id="login_btn">Login</button>
+                <Link to="/login" className="btn ml-4" id="login_btn">Login</Link>
 
                     <span id="cart" className="ml-3">Cart</span>
                     <span className="ml-1" id="cart_count">2</span>
