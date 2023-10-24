da03af67617214e6041b4efcf743e799b85a6859
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-24 09:27:28 +0800

    add to cart local storage

--- frontend/src/Components/Product/ProductDetails.js
+++ frontend/src/Components/Product/ProductDetails.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 
 
 
-const ProductDetails = ({ match }) => {
+const ProductDetails = ({ addItemToCart, cartItems }) => {
 
     const [loading, setLoading] = useState(true)
     const [product, setProduct] = useState({})
@@ -45,6 +45,9 @@ const ProductDetails = ({ match }) => {
         const qty = count.valueAsNumber - 1;
         setQuantity(qty)
     }
+    const addToCart =  async () => {
+        await addItemToCart(id, quantity);
+    }
 
     useEffect(() => {
         productDetails(id)
@@ -52,6 +55,8 @@ const ProductDetails = ({ match }) => {
         //     alert.error(error);
         // }
     }, [id,]);
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems))
 
     return (
         <Fragment>
@@ -90,7 +95,7 @@ const ProductDetails = ({ match }) => {
                                 {/* <span className="btn btn-primary plus"> +</span> */}
                                 <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
                             </div>
-                            <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4"  >Add to Cart</button>
+                            <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4" disabled={product.stock === 0} onClick={addToCart}>Add to Cart</button>
 
                             <hr />
 
