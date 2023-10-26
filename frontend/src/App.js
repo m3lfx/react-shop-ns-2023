11ed6a80e690a2865e98ecc81d99762d5ef35c51
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-26 09:17:50 +0800

    payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import "react-toastify/dist/ReactToastify.css";
 import axios from 'axios';
 import Shipping from './Components/Cart/Shipping';
 import ConfirmOrder from './Components/Cart/ConfirmOrder';
+import Payment from './Components/Cart/Payment';
 
 function App() {
   const [state, setState] = useState({
@@ -112,6 +113,7 @@ function App() {
           />}
           />
           <Route path="/confirm" element={<ConfirmOrder cartItems={state.cartItems} shippingInfo={state.shippingInfo} />}  />
+          <Route path="/payment" element={<Payment cartItems={state.cartItems} shippingInfo={state.shippingInfo} />}  />
         </Routes>
         <Footer />
       </Router>
---
+++ frontend/src/Components/Cart/Payment.js
@@ -0,0 +1,116 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import MetaData from '../Layout/Metadata'
+import CheckoutSteps from './CheckoutSteps'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { getToken } from '../../utils/helpers';
+
+
+const Payment = ({cartItems, shippingInfo}) => {
+    const [loading, setLoading] = useState(true)
+    let navigate = useNavigate();
+    const order = {
+        orderItems: cartItems,
+        shippingInfo
+    }
+
+    const orderInfo = JSON.parse(sessionStorage.getItem('orderInfo'));
+    if (orderInfo) {
+        order.itemsPrice = orderInfo.itemsPrice
+        order.shippingPrice = orderInfo.shippingPrice
+        order.taxPrice = orderInfo.taxPrice
+        order.totalPrice = orderInfo.totalPrice
+    }
+
+    const createOrder = async (order) => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${getToken()}`
+                }
+            }
+            const { data } = await axios.post(`${process.env.REACT_APP_API}/api/v1/order/new`, order, config)
+            // setIsUpdated(data.success)
+            setLoading(false)
+            toast.success('order created', {
+                position: toast.POSITION.BOTTOM_RIGHT
+            });
+           
+            sessionStorage.removeItem('orderInfo')
+            navigate('/success')
+    
+        } catch (error) {
+            toast.error(error.response.data.message, {
+                position: toast.POSITION.BOTTOM_RIGHT
+            });
+           }
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        document.querySelector('#pay_btn').disabled = true;
+        order.paymentInfo = {
+            id: 'pi_1DpdYh2eZvKYlo2CYIynhU32',
+            status: 'succeeded'
+        }
+        createOrder(order)
+        navigate('/success')
+      }
+
+    return (
+        <Fragment>
+            <MetaData title={'Payment'} />
+            <CheckoutSteps shipping confirmOrder payment />
+            <div className="row wrapper">
+                <div className="col-10 col-lg-5">
+                    <form className="shadow-lg" onSubmit={submitHandler}>
+                        <h1 className="mb-4">Card Info</h1>
+                        <div className="form-group">
+                            <label htmlFor="card_num_field">Card Number</label>
+                            <input
+                                type="text"
+                                id="card_num_field"
+                                className="form-control"
+                                
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="card_exp_field">Card Expiry</label>
+                            <input
+                                type="text"
+                                id="card_exp_field"
+                                className="form-control"
+                               
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="card_cvc_field">Card CVC</label>
+                            <input
+                                type="text"
+                                id="card_cvc_field"
+                                className="form-control"
+                                
+                            />
+                        </div>
+                        <button
+                            id="pay_btn"
+                            type="submit"
+                            className="btn btn-block py-3"
+                        >
+                            Pay {` - ${orderInfo && orderInfo.totalPrice}`}
+                        </button>
+
+                    </form>
+                </div>
+            </div>
+
+        </Fragment>
+    )
+}
+
+export default Payment
