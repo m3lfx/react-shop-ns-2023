42c7fecca9db7af65fa064615a64558cab4089de
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-03 09:03:26 +0800

    redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux'
+import store from './store'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <ToastContainer />
+    <Provider store={store} >
+      <App />
+      <ToastContainer />
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
