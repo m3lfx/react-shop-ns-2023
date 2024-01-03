a42496ac0ab1d696962c53e1f7df1c870fe4eff1
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-03 09:15:02 +0800

    add reducer to store

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -9,7 +9,7 @@ export const productsReducer = (state = { products: [] }, action) => {
         case ALL_PRODUCTS_REQUEST:
             return {
                 loading: true,
-                propducts: []
+                products: []
             }
         case ALL_PRODUCTS_SUCCESS:
             return {
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,10 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+import { productReducer } from './reducers/productReducers'
+
 const reducer = combineReducers({
+    products: productReducer,
 
 })
 
