65570ff6e6a67e20c5e1ce8ae95eec1bbb5d05fc
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-06 08:26:56 +0800

    product details reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,9 @@ import {
     ALL_PRODUCTS_REQUEST,
     ALL_PRODUCTS_SUCCESS,
     ALL_PRODUCTS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
     CLEAR_ERRORS
 } from '../constants/productConstants'
 export const productsReducer = (state = { products: [] }, action) => {
