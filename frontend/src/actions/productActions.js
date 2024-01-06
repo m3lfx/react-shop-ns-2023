a86b9aeb66b5caeabd5ebd87fc335b37b48a9823
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-06 09:59:52 +0800

    delete/update product action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -12,6 +12,13 @@ import {
 	NEW_PRODUCT_REQUEST,
 	NEW_PRODUCT_SUCCESS,
 	NEW_PRODUCT_FAIL,
+	DELETE_PRODUCT_REQUEST,
+	DELETE_PRODUCT_SUCCESS,
+	DELETE_PRODUCT_FAIL,
+	UPDATE_PRODUCT_REQUEST,
+	UPDATE_PRODUCT_SUCCESS,
+	UPDATE_PRODUCT_FAIL,
+
 	CLEAR_ERRORS
 } from '../constants/productConstants';
 import { getToken, } from '../utils/helpers'
@@ -104,6 +111,46 @@ export const newProduct = (productData) => async (dispatch) => {
 			payload: error.response.data.message
 		})
 	}
+}
+
+export const deleteProduct = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: DELETE_PRODUCT_REQUEST })
+        const { data } = await axios.delete(`${process.env.REACT_APP_API}/api/v1/admin/product/${id}`, {
+            'Authorization': `Bearer ${getToken()}`
+        })
+        dispatch({
+            type: DELETE_PRODUCT_SUCCESS,
+            payload: data.success
+        })
+    } catch (error) {
+        dispatch({
+            type: DELETE_PRODUCT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const updateProduct = (id, productData) => async (dispatch) => {
+    try {
+        dispatch({ type: UPDATE_PRODUCT_REQUEST })
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+				'Authorization': `Bearer ${getToken()}`
+            }
+        }
+        const { data } = await axios.put(`${process.env.REACT_APP_API}/api/v1/admin/product/${id}`, productData, config)
+        dispatch({
+            type: UPDATE_PRODUCT_SUCCESS,
+            payload: data.success
+        })
+    } catch (error) {
+        dispatch({
+            type: UPDATE_PRODUCT_FAIL,
+            payload: error.response.data.message
+        })
+    }
 }
 
 export const clearErrors = () => async (dispatch) => {
