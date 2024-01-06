148a31d348a7c9acdb9d1e438450047012f6ee4f
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-06 10:56:46 +0800

    auth reducers

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -40,3 +40,56 @@ import {
     DELETE_USER_FAIL,
     CLEAR_ERRORS
 } from '../constants/userConstants'
+
+export const authReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case REGISTER_USER_REQUEST:
+        case LOGIN_REQUEST:
+        case LOAD_USER_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                isAuthenticated: false,
+            }
+        case REGISTER_USER_SUCCESS:
+        case LOGIN_SUCCESS:
+        case LOAD_USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isAuthenticated: true,
+                user: action.payload
+            }
+        case REGISTER_USER_FAIL:
+        case LOGIN_FAIL:
+        case LOAD_USER_FAIL:
+            return {
+                ...state,
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload
+            }
+
+        case LOGOUT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isAuthenticated: false,
+                user: null
+            }
+        case LOGOUT_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state
+    }
+}
