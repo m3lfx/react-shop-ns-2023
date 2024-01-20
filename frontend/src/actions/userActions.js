a663fa3d89302b10577244b3baa40a7fb5aa732e
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-20 09:48:32 +0800

    user actions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -200,6 +200,87 @@ export const forgotPassword = (email) => async (dispatch) => {
 
 }
 
+export const allUsers = () => async (dispatch) => {
+    try {
+        dispatch({ type: ALL_USERS_REQUEST })
+        const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/admin/users`, {
+            //AxiosRequestConfig parameter
+            withCredentials: true //correct
+          })
+        dispatch({
+            type: ALL_USERS_SUCCESS,
+            payload: data.users
+        })
+    } catch (error) {
+        dispatch({
+            type: ALL_USERS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const getUserDetails = (id) => async (dispatch) => {
+    
+    try {
+        dispatch({ type: USER_DETAILS_REQUEST })
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }, withCredentials: true
+
+        }
+        const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/admin/user/${id}`,config)
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data.user
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const updateUser = (id, userData) => async (dispatch) => {
+    try {
+        dispatch({ type: UPDATE_USER_REQUEST })
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }, withCredentials: true
+
+        }
+        const { data } = await axios.put(`${process.env.REACT_APP_API}/api/v1/admin/user/${id}`, userData, config)
+        dispatch({
+            type: UPDATE_USER_SUCCESS,
+            payload: data.success
+        })
+    } catch (error) {
+        dispatch({
+            type: UPDATE_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const deleteUser = (id) => async (dispatch) => {
+
+    try {
+        dispatch({ type: DELETE_USER_REQUEST })
+        const { data } = await axios.delete(`${process.env.REACT_APP_API}/api/v1/admin/user/${id}`, {withCredentials: true})
+        dispatch({
+            type: DELETE_USER_SUCCESS,
+            payload: data.success
+        })
+    } catch (error) {
+        dispatch({
+            type: DELETE_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
 export const clearErrors = () => async (dispatch) => {
 	dispatch({
 		type: CLEAR_ERRORS
